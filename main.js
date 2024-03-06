09ca21524583315c0d495a091d056a690e148c42
evY000 <000evan001@gmail.com>
2024-03-05 23:35:04 -0500

    aswdghnjk

--- main.js
+++ main.js
@@ -1,36 +1,92 @@
 const DOMSelectors ={
     btn : document.querySelectorAll(".button"),
     card : document.querySelectorAll(".Card"),
-    alters : document.querySelector(".alter")
+    alters : document.querySelector(".alter"),
+    b1 :  document.querySelector("b1"),
+    b2 :  document.querySelector("b2"),
+    b3 :  document.querySelector("b3"),
+    b4:  document.querySelector("b4"),
+    b5 :  document.querySelector("b5"),
+    b6:  document.querySelector("b6"),
+    b7 :  document.querySelector("b7"),
+    b8:  document.querySelector("b8"),
+    b9 :  document.querySelector("b9"),
+
 }
-let set = ["", "", "", "", "", "", "", "", ""]
-const Win = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+
 let TurnX = true
 let TurnO = false
-DOMSelectors.btn.forEach(alter => {
-    alter.addEventListener("click", ()=>{
-        if(TurnX === true, alter.innerHTML === 'O'){
+DOMSelectors.btn.forEach(button => {
+    button.addEventListener("click", ()=>{
+        if(TurnX === true, button.innerHTML === 'O'){
             alert('Not A valid Move')
             return;           }
         if(TurnX === true){
-           alter.innerHTML = 'X'
+           button.innerHTML = 'X'
            TurnX = false
            TurnO = true
-           Match()
-            
-              
+           Match()  
             }
             else{
-                if(TurnO === true, alter.innerHTML === 'X'){
+                if(TurnO === true, button.innerHTML === 'X'){
                     alert('Not A valid Move')
                     return;}
-                alter.innerHTML = 'O'
+              button.innerHTML = 'O'
                 TurnX = true
                 TurnO = false
                 Match()
             }
+            function Match(){
+            if(b1.innerHTML === 'X' && b2.innerHTML === 'X' && b3.innerHTML ==='X'){
+               alert('X Has Won!')
+                };
+
+            if(b4.innerHTML === 'X' && b5.innerHTML === 'X'&& b6.innerHTML ==='X'){
+                alert('X Has Won!')};
+
+            if(b7.innerHTML === 'X' && b8.innerHTML === 'X' && b9.innerHTML ==='X'){
+                    alert('X Has Won!')};
+
+            if(b1.innerHTML === 'X'&& b4.innerHTML === 'X' && b7.innerHTML ==='X'){
+                        alert('X Has Won!')};
+
+            if(b2.innerHTML === 'X' && b5.innerHTML === 'X' && b8.innerHTML ==='X'){
+                        alert('X Has Won!')};
+
+            if(b3.innerHTML === 'X'&& b6.innerHTML === 'X'&& b9.innerHTML ==='X'){
+                        alert('X Has Won!')};
+
+            if(b1.innerHTML === 'X'&& b5.innerHTML === 'X'&& b9.innerHTML ==='X'){
+                        alert('X Has Won!')};
+
+            if(b3.innerHTML === 'X'&& b5.innerHTML === 'X'&& b7.innerHTML ==='X'){
+                        alert('X Has Won!')};
+            if(b1.innerHTML === 'O' && b2.innerHTML === 'O' && b3.innerHTML ==='O'){
+                            alert('O Has Won!')
+                             };
+             
+                         if(b4.innerHTML === 'O' && b5.innerHTML === 'O'&& b6.innerHTML ==='O'){
+                             alert('O Has Won!')};
+             
+                         if(b7.innerHTML === 'O' && b8.innerHTML === 'O' && b9.innerHTML ==='O'){
+                                 alert('O Has Won!')};
+             
+                         if(b1.innerHTML === 'O'&& b4.innerHTML === 'O' && b7.innerHTML ==='O'){
+                                     alert('O Has Won!')};
+             
+                         if(b2.innerHTML === 'O' && b5.innerHTML === 'O' && b8.innerHTML ==='O'){
+                                     alert('O Has Won!')};
+             
+                         if(b3.innerHTML === 'O'&& b6.innerHTML === 'O'&& b9.innerHTML ==='O'){
+                                     alert('O Has Won!')};
+             
+                         if(b1.innerHTML === 'O'&& b5.innerHTML === 'O'&& b9.innerHTML ==='O'){
+                                     alert('O Has Won!')};
+             
+                         if(b3.innerHTML === 'O'&& b5.innerHTML === 'O'&& b7.innerHTML ==='O'){
+                                     alert('O Has Won!')};
+             
+
+                }
         })
 });
-function Match(){
-    console.log('hi')
-}
